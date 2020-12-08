d6a2f906a395355bbac3fe27a05abc65f1025cf2
CR AHSAN QURESHI <amohd68910@gmail.com>
2020-12-09 00:26:22 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const date = new Date()
+const tym = new Date()
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  < >
+    <div className="bg">
+      <img className="image1" src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg" alt="image1" />
+      <p style={{ color: "whitesmoke" }} className="name"> AHSAN QURESHI </p>
+      <p style={{ color: "whitesmoke" }}>{date.toLocaleDateString()}</p>
+      <p style={{ color: "whitesmoke" }}className="tym">{tym.toLocaleTimeString()}</p>
+      <img style={{width:"500px"}} src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg" alt="image1" />
+      <br></br>
+      <button className="btn">LIKE</button>
+      <button className="btn" >COMMENT</button>
+      <button className="btn">SHARE</button>
+      <br></br>
+      <img className="image1" src="https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg" alt="image2" />
+      <p style={{ color: "whitesmoke" }}  className="name"> SADIQ HUSSAIN </p>
+      <p style={{ color: "whitesmoke" }} >{date.toLocaleDateString()}</p>
+      <p style={{ color: "whitesmoke" }} className="tym">{tym.toLocaleTimeString()}</p>
+      
+      <img style={{width:"500px"}} src="https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg" alt="image2" />
+      <br></br>
+      <button className="btn">LIKE</button>
+      <button className="btn" >COMMENT</button>
+      <button className="btn">SHARE</button>" 
+    <br></br>
+      <img className="image1" src="https://images.pexels.com/photos/956981/milky-way-starry-sky-night-sky-star-956981.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="image1" />
+      <p style={{ color: "whitesmoke" }}  className="name"> AHMED QURESHI </p>
+      <p style={{ color: "whitesmoke" }}>{date.toLocaleDateString()}</p>
+      <p style={{ color: "whitesmoke" }} className="tym">{tym.toLocaleTimeString()}</p>
+      
+      <img style={{width:"500px"}} src="https://images.pexels.com/photos/956981/milky-way-starry-sky-night-sky-star-956981.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="image1" />
+      <br></br>
+      <button className="btn">LIKE</button>
+      <button className="btn" >COMMENT</button>
+      <button className="btn">SHARE</button>
+    </div>
+  </>,
+  document.querySelector('#root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
